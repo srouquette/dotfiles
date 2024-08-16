fa85e17871de29ecd729ee69574dd5f280853952
Syl ⏚ <srouquette@gmail.com>
2024-08-16 11:33:21 +0200

    [msys2] switch to UCRT64 by default always install git-for-windows. optimized packages install with a single command line

--- .chezmoiscripts/msys2/run_onchange_before_install-git.sh
+++ .chezmoiscripts/msys2/run_onchange_before_install-git.sh
@@ -1,36 +1,28 @@
 #!/usr/bin/env bash
 set -eufo pipefail
 
-if [ "$MSYSTEM" = "MINGW64" ]; then
-  # remove the other git package
-  pacman -Qqs ^git$ &>/dev/null && pacman -Rsnc --noconfirm git
+# exit if git is already installed
+pacboy -Qqs git:x &>/dev/null && exit 0
 
-  # install pactoys, for pacboy, if it wasn't already installed
-  pacman -Qqs pactoys &>/dev/null || pacman -S --noconfirm pactoys
+# remove the git package from the main repo
+pacman -Qqs ^git$ &>/dev/null && pacman -Rsnc --noconfirm git
 
-  # exit if git is already installed
-  pacboy -Qqs git:p &>/dev/null && exit 0
+# install pactoys, for pacboy, if it wasn't already installed
+pacman -Qqs pactoys &>/dev/null || pacman -S --noconfirm pactoys
 
-  # add git-for-windows repo (custom build)
-  REPO_NAME='git-for-windows'
-  REPO_ENTRY="[$REPO_NAME]
-  Server = https://wingit.blob.core.windows.net/x86-64"
+# add git-for-windows repo (custom build)
+REPO_NAME='git-for-windows'
+REPO_ENTRY="[$REPO_NAME]
+Server = https://wingit.blob.core.windows.net/x86-64"
 
-  if ! grep -q "\[${REPO_NAME}\]" /etc/pacman.conf; then
-    echo -e "\n${REPO_ENTRY} " >> /etc/pacman.conf
-    pacman -Sy --noconfirm
-  fi
-
-  pacboy -S --noconfirm git:p
-
-
-else
-  # exit if a version of git is already installed
-  pacman -Qs git &>/dev/null && exit 0
-
-  echo "msys2 is running with the environment ${MSYSTEM}."
-  echo 'Installing git for this environment instead of the faster git-for-windows, which is only available on MINGW64.'
-
-  pacman -S --noconfirm git
-
+if ! grep -q "\[${REPO_NAME}\]" /etc/pacman.conf; then
+  echo -e "\n${REPO_ENTRY} " >> /etc/pacman.conf
+  pacman -Sy --noconfirm
 fi
+
+pacboy -S --noconfirm git:x
+
+alias git='PKG_CONFIG_PATH=/mingw64/lib/pkgconfig:/mingw64/share/pkgconfig && \
+PKG_CONFIG_SYSTEM_INCLUDE_PATH=/mingw64/include && \
+PKG_CONFIG_SYSTEM_LIBRARY_PATH=/mingw64/lib && \
+git'
--- .chezmoiscripts/msys2/run_onchange_before_install-packages.sh
+++ .chezmoiscripts/msys2/run_onchange_before_install-packages.sh
@@ -13,7 +13,7 @@ pacboy_packages=(
     bat:p
     fzf:p
     jq:p
-    lsd:p
+    eza:p
     python-tldr:p
     starship:p
     vivid:p
@@ -21,13 +21,17 @@ pacboy_packages=(
 
 installed=$(pacman -Qqe)
 
+filtered=()
 for pkg in "${pacman_packages[@]}"; do
-  [[ $installed == *${pkg}* ]] || pacman -S --noconfirm $pkg
+  [[ $installed == *${pkg}* ]] || filtered+=("$pkg")
 done
+[ ${#filtered[@]} -eq 0 ] || pacman -S --noconfirm ${filtered[@]}
 
+filtered=()
 for pkg in "${pacboy_packages[@]}"; do
-  [[ $installed == *${pkg%:*}* ]] || pacboy -S --noconfirm $pkg
+  [[ $installed == *${pkg%:*}* ]] || filtered+=("$pkg")
 done
+[ ${#filtered[@]} -eq 0 ] || pacboy -S --noconfirm ${filtered[@]}
 
 # --assume-installed doesn't seem to work with pacboy
 [[ $installed == *delta* ]] || pacman -S --noconfirm --assume-installed git ${MINGW_PACKAGE_PREFIX}-delta
