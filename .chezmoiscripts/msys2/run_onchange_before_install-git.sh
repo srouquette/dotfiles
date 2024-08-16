e70ce13baa3845501b425592bddadcaafbec74eb
Syl ⏚ <srouquette@gmail.com>
2024-08-16 18:45:28 +0200

    [msys2] remove completely the way to manage 2 msystem some apps wouldn't work anymore, would have to do aliases, but then it would break zsh plugins not worth the hassle.

--- .chezmoiscripts/msys2/run_onchange_before_install-git.sh
+++ .chezmoiscripts/msys2/run_onchange_before_install-git.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 set -eufo pipefail
 
-# exit if git is already installed
-pacboy -Qqs git:x &>/dev/null && exit 0
+if [ "$MSYSTEM" = 'MINGW64' ]; then
+  pacboy -Qqs git:x &>/dev/null && exit 0
 
-# remove the git package from the main repo
-pacman -Qqs ^git$ &>/dev/null && pacman -Rsnc --noconfirm git
+  # remove the git package from the main repo
+  pacman -Qqs ^git$ &>/dev/null && pacman -Rsnc --noconfirm git
 
-# install pactoys, for pacboy, if it wasn't already installed
-pacman -Qqs pactoys &>/dev/null || pacman -S --noconfirm pactoys
+  # install pactoys, for pacboy, if it wasn't already installed
+  pacman -Qqs pactoys &>/dev/null || pacman -S --noconfirm pactoys
 
-# add git-for-windows repo (custom build)
-REPO_NAME='git-for-windows'
-REPO_ENTRY="[$REPO_NAME]
-Server = https://wingit.blob.core.windows.net/x86-64"
+  REPO_NAME='git-for-windows'
+  REPO_ENTRY="[$REPO_NAME]
+  Server = https://wingit.blob.core.windows.net/x86-64"
 
-if ! grep -q "\[${REPO_NAME}\]" /etc/pacman.conf; then
-  echo -e "\n${REPO_ENTRY} " >> /etc/pacman.conf
-  pacman -Sy --noconfirm
+  if ! grep -q "\[${REPO_NAME}\]" /etc/pacman.conf; then
+    echo -e '\n${REPO_ENTRY}' >> /etc/pacman.conf
+    pacman -Sy --noconfirm
+  fi
+
+  pacboy -S --noconfirm git:x
+
+else
+
+  pacman -Qqs '^git$' &>/dev/null && exit 0
+  pacboy -Qqs 'git:x' &>/dev/null && exit 0
+
+  echo "msys2 is running with the environment ${MSYSTEM}."
+  echo 'it will install git for this environment instead of the faster git-for-windows, which is only available on MINGW64.'
+
+  pacman -S --noconfirm git
+
 fi
-
-pacboy -S --noconfirm git:x
