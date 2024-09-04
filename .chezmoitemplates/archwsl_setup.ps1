aded06038954af8e36bc1d63ac8913a0f08bda5e
Syl ⏚ <srouquette@gmail.com>
2024-09-04 20:10:49 +0200

    use USERNAME for WSL, easier to setup

--- .chezmoitemplates/archwsl_setup.ps1
+++ .chezmoitemplates/archwsl_setup.ps1
@@ -2,7 +2,7 @@
 {{ $script_root := joinPath .chezmoi.sourceDir ".internals" "wsl" "setup_as_root.sh" }}
 {{ $script_user := joinPath .chezmoi.sourceDir ".internals" "wsl" "setup_as_user.sh" }}
 
-Invoke-Command -ScriptBlock { {{ $cmd }} runp {{ $script_root }} {{ .username }} }
+Invoke-Command -ScriptBlock { {{ $cmd }} runp {{ $script_root }} $Env:USERNAME }
 Invoke-Command -ScriptBlock { {{ $cmd }} config --append-path false }
-Invoke-Command -ScriptBlock { {{ $cmd }} config --default-user {{ .username }} }
-Invoke-Command -ScriptBlock { {{ $cmd }} runp {{ $script_user }} {{ .username }} }
+Invoke-Command -ScriptBlock { {{ $cmd }} config --default-user $Env:USERNAME }
+Invoke-Command -ScriptBlock { {{ $cmd }} runp {{ $script_user }} $Env:USERNAME }
